27bdba21a8488e94815d925db92f80732063f5f6
Kkkakania <200867803+Kkkakania@users.noreply.github.com>
2026-07-09 21:40:02 +0800

    Match privacy platform markers case-insensitively

--- scripts/check_static_quality.sh
+++ scripts/check_static_quality.sh
@@ -58,6 +58,7 @@ run_step "workflow maintenance settings" ./scripts/check_workflows.sh
 run_step "forbidden file self-test" ./scripts/check_forbidden_files_selftest.sh
 run_step "privacy self-test" ./scripts/check_privacy_selftest.sh
 run_step "forbidden file scan" ./scripts/check_forbidden_files.sh
+run_step "privacy pattern self-test" ./scripts/check_privacy_patterns_selftest.sh
 run_step "privacy scan" ./scripts/check_privacy.sh
 run_step "provenance pattern self-test" ./scripts/check_provenance_patterns_selftest.sh
 run_step "provenance scan" ./scripts/check_provenance.sh
